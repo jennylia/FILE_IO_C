4746dbf0b8c5e26833c8f76603b85b5dab15bd9b
Jenny Lian <jennylian100@gmail.com>
2016-04-02 18:31:37 -0700

    Part 1 is done done done done

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+/*
+ * Play 
+ * 1. Create a dir if a file doesn't exist
+ * 2. Then create a file in it
+ * 3. make the name of a file encrupted*/
+
+#define NAME "FOLDER"
 int main(){
-  printf("hello\n");
+  DIR * dp;
+  int status;
+  if ((dp = opendir(NAME)) == NULL){
+      printf("what the hell\n"); 
+      status = mkdir(NAME, 0777);
+      if (status < 0){
+        printf("shit\n");
+      }else{
+        printf("yay\n");
+      } 
+  }
+
 }
